331ffadc727122735e2c4b9dfba9f5b3a31dabf6
guy <ethannathanduvall2013@gmail.com>
2024-08-07 20:21:52 -0400

    Update ai.lua

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,9 +3,9 @@ return {
 		"frankroeder/parrot.nvim",
 		tag = "v0.3.10",
 		dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
-		config = function()
-			require("parrot").setup({
-				providers = 
+		--config = function()
+			--require("parrot").setup({
+				--providers = 
 					--openai = {
 						--api_key = os.getenv("OPENAI_API_KEY"),
 					--},
@@ -13,8 +13,8 @@ return {
 						--api_key = os.getenv("GEMINI_API_KEY"),
 					--},
 					--ollama = {}, -- provide an empty list to make provider available
-				},
-			})
-		end,
+				--},
+			--})
+		--end,
 	},
 }
